63b4f0b3149ccbbf6623ac2659e426450e21f848
Vanessa Patterson <vpatt1031@gmail.com>
2021-02-27 18:58:07 -0500

    created needed files and directories

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Cheddar Melt');
+INSERT INTO burgers (burger_name) VALUES ('Sloppy Jose');
+INSERT INTO burgers (burger_name) VALUES ('Black and Blue');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Chicken Melt');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Mushroom Swiss');
+INSERT INTO burgers (burger_name) VALUES ('All American Bacon');
